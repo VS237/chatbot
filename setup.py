d826ab94f8b44abbc3ff8a4d21f93aa48af6dd62
VS237 <victorinsonhofo@gmail.com>
2025-08-05 01:21:04 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="banking_chatbot",
+  version="0.1.0",
+  author="Sonhofo Victorin",
+  author_email="victorinsonhofo@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
